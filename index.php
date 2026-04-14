f54b987a3ed59544ca558d066e07c0404ec10702
ashtonstu <ashton.stuart@snhu.edu>
2026-04-14 19:29:17 -0400

    Added a test

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ include('Includes/header.php');
 <section class="content">
   <h1>Welcome to INFM 332 Spring 2026</h1>
   <p>This class project will use PHP and MySQL.</p>
+  <p> Test </p>
 </section>
 
 <?php
